85e2c37fd46e72fb034681fd893418a8635eb1cc
Nut <nuttapon.tavee@gmail.com>
2017-09-15 17:31:43 +0700

    update

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,7 +1,22 @@
+import {
+  REGISTER_SUCCESS,
+  LOGIN_SUCCESS,
+  LOGOUT
+} from 'Actions'
+import { getToken } from 'Lib';
+
 const initialState = {
-  token: []
+  token: getToken()
 };
 
-export default (state = initialState) => {
-  return state
+export default (state = initialState, action) => {
+  switch(action.type) {
+    case LOGIN_SUCCESS:
+    case REGISTER_SUCCESS:
+      return { token: action.payload.token };
+    case LOGOUT:
+      return { token: null };
+    default:
+      return state
+  }
 }
